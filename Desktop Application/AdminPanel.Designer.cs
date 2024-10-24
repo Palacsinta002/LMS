2010add53020526d0bc7f62b3a817a74cf291a02
Vágvölgyi Máté <vagvolgyimate@gszi.edu.hu>
2024-10-24 14:25:46 +0200

    2024-10-24 14:25:45

--- Desktop Application/AdminPanel.Designer.cs
+++ Desktop Application/AdminPanel.Designer.cs
@@ -28,22 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            close_btn = new Label();
+            label1 = new Label();
+            panel1 = new Panel();
             SuspendLayout();
+            // 
+            // close_btn
+            // 
+            close_btn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            close_btn.AutoSize = true;
+            close_btn.Cursor = Cursors.Hand;
+            close_btn.Font = new Font("Yu Gothic UI Semibold", 12F);
+            close_btn.Location = new Point(752, 9);
+            close_btn.Name = "close_btn";
+            close_btn.Size = new Size(20, 21);
+            close_btn.TabIndex = 7;
+            close_btn.Text = "X";
+            close_btn.Click += CloseWindow;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(314, 111);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 8;
+            label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(80, 77, 180);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 422);
+            panel1.TabIndex = 9;
             // 
             // adminPanel
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(784, 461);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Controls.Add(close_btn);
             MinimumSize = new Size(800, 500);
             Name = "adminPanel";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Admin Panel";
             WindowState = FormWindowState.Maximized;
-            Load += adminPanel_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label close_btn;
+        private Label label1;
+        private Panel panel1;
     }
 }
--- Desktop Application/AdminPanel.cs
+++ Desktop Application/AdminPanel.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Desktop_Application
+﻿namespace Desktop_Application
 {
     public partial class adminPanel : Form
     {
@@ -17,9 +7,9 @@ namespace Desktop_Application
             InitializeComponent();
         }
 
-        private void adminPanel_Load(object sender, EventArgs e)
+        private void CloseWindow(object sender, EventArgs e)
         {
-            
+            this.Close();
         }
     }
 }
