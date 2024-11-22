66607950bb92b68a5ab64f8a125d11bd8f165bc3
Tóth Zoltán <78497888+PhantoMan26@users.noreply.github.com>
2024-11-22 11:21:39 +0100

    2024-11-22 11:21:38

---
+++ 20241122/FormValidation.php
@@ -0,0 +1,21 @@
+<?php
+    require "config/config.php";
+    
+    $username = "";
+    $password = "";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $sql = "SELECT users.username, users.password FROM users WHERE users.username = '$username' AND users.password = '$password'";
+    
+        $result = $conn->query($sql);
+    
+        if($result->num_rows > 0){
+            header('Location: ./books.php', true, 418);
+            
+            exit();
+        }
+    
+    }
