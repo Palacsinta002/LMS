ae732af2a9ec3c106d76a092f90a1024a1d03d08
Toth Zoltan <tot.zol0226@gmail.com>
2024-12-04 19:09:34 +0100

    2024-12-04 19:09:34

---
+++ Website/FrontEnd/firstReactApp/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from "./Components/Header"
+import Main from './Components/Main'
+
+function App() {
+
+  return (
+    <>
+      <section className="Hero">
+        <Header />
+        <Main />
+      </section>
+      <section className="Browse">
+        
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ Website/FrontEnd/firstReactApp/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../index.css"
+
+export default function Header() {
+  return (
+    <header className="header">
+        <h1 className="title">LMS</h1>
+        <div className="logReg">
+            <a href="" className="signup">Sign Up</a>
+            <a href="" className="signin">Sign In</a>
+        </div>
+    </header>
+  )
+}
