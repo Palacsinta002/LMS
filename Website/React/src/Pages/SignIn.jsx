0669e7ee43340371670a5d943483021a6c1174bc
Toth Zoltan <tot.zol0226@gmail.com>
2024-12-13 15:20:35 +0100

    2024-12-13 15:20:34

--- Website/React/src/Pages/SignIn.jsx
+++ Website/React/src/Pages/SignIn.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import Login from '../Components/Login'
 import "../index.css"
+import { Link } from 'react-router-dom'
+import "./SignIn.css"
 
 export default function SignIn() {
   return (
     <div className="login">
-        <h1 className="login-header">Library Management System</h1>
-        <Login />
+      <h1 className="login-header">Library Management System</h1>
+      <div className="login-card">
+        <i className="fa fa-user"></i>
+        <h1>Login</h1>
+        <form action="">
+          <label>Username</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="password" />
+          <span><Link className="link1">Forgot your password?</Link></span>
+          <input type="submit" value="Sign in" />
+          <span><Link className="link2">Create new account?</Link></span>
+        </form>
+      </div>
     </div>
   )
 }
