6768e6f4b789ea3413f4a800ae347c92e2ed6f75
Toth Zoltan <tot.zol0226@gmail.com>
2024-12-13 15:33:28 +0100

    Merge branch 'main' of https://github.com/Palacsinta002/WIP

--- Website/React/src/Pages/Home.jsx
+++ Website/React/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Main from '../Components/Main'
 import HeroBrowseBooks from '../Components/HeroBrowseBooks'
+import "./Home.css"
 
 export default function Home() {
   return (
