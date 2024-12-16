24f4af12e698cf5cf63035925411c6e6474e77f3
Toth Zoltan <tot.zol0226@gmail.com>
2024-12-16 16:49:47 +0100

    asdf

--- Website/React/src/Pages/SignUp.jsx
+++ Website/React/src/Pages/SignUp.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./SignUp.css"
 
 export default function SignUp() {
   return (
-    <div>
-        SignUp
+    <div className="register">
+      <h1 className="register-header">Library Management System</h1>
+      <div className="register-container">
+        <i className="fa fa-user"></i>
+        <h1>Register Account</h1>
+        <form action="">
+          <div className="register-card-holder">
+            <div className="register-card">
+              <label>Email</label>
+              <input type="email" />
+            </div>
+
+            <div className="register-card">
+              <label>Username</label>
+              <input type="text" />
+            </div>
+
+            <div className="register-card">
+              <label>First name</label>
+              <input type="text" />
+            </div>
+
+            <div className="register-card">
+              <label>Last name</label>
+              <input type="text" />
+            </div>
+
+            <div className="register-card">
+              <label>Password</label>
+              <input type="password" />
+            </div>
+
+            <div className="register-card">
+              <label>Password again</label>
+              <input type="password" />
+            </div>
+          </div>
+          
+          <div className="actions">
+            <input type="submit" value="Sign in" />
+            <span><Link className="link2">I already have an account!</Link></span>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
