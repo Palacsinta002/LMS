cacec8bfe166aed38674d62f70fc137e20dd54e8
Tóth Zoltán <78497888+PhantoMan26@users.noreply.github.com>
2025-01-24 12:08:52 +0100

    2025-01-24 12:08:52

--- Website/React/src/Pages/SignIn.jsx
+++ Website/React/src/Pages/SignIn.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import { Link } from 'react-router-dom'
+import { Link, resolvePath } from 'react-router-dom'
 import "./SignIn.css"
 
 export default function SignIn() {
@@ -19,57 +19,24 @@ export default function SignIn() {
   /*function TogglePassword(){
     setShowPassword(!showpassword);
   }*/
-  const handleSubmit = async () => {
-    const requestMethod = {
-      method: "POST",
-      header: {"Content-Type": "application/json"},
-      body: JSON.stringify({username: username, password: password}),
-      mode: "CORS"
-    };
-    await axios.post("http://localhost/LMS/Website/PHP/realproject/users/userapi.php", requestMethod)
-    .then(response => console.log(JSON.stringify(response)))
-    .catch(error => console.log(error));
-
-<<<<<<< HEAD
-  function handleSubmit(event){
-    event.preventDefault();
-    axios
-    .post("localhost/5173/register.php",{
+  
+  async function HandleSubmit(e) {
+    e.preventDefault();
+    await axios.post("http://localhost/LMS/Website/PHP/realproject/users/userapi.php", {
+      header:{
+        "Allow-Control-Allow-Origin" : "*",
+        "Content-Type": "application/json"
+      },
+      proxy:{
+        port: 5173
+      },
       username: username,
       password: password
     })
-    .then((response) => {
-      if(response.data.status === "success"){
-        localStorage.setItem("loggedIn", true);
-        localStorage.setItem("userData",
-          JSON.stringify(response.data.data));
-        window.location.href = "/home"
-      }
-      else{
-        setError(response.data.message);
-      }
-    })
-    .catch((error) => {
-=======
-    /*try {
-      const response = await axios.post("", {
-        username,
-        password,
-        method: "POST",
-      },
-      {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      setError(response.data.message);
-      console.log(response.data.message);
-    } catch (error) {
-      setError("Something went wrong...");
->>>>>>> 083ad848f8e951d5466cdb7d764de078ee1b69de
-      console.error(error);
-    }*/
+    .then(response => console.log(JSON.stringify(response)))
+    .catch(error => console.log(error))
   }
+
   return (
     <div className="login">
       <h1 className="login-header">Library Management System</h1>
@@ -82,7 +49,7 @@ export default function SignIn() {
           <label>Password</label>
           <input type="password" onChange={PasswordOnChange}/>
           <span><Link className="link1">Forgot your password?</Link></span>
-          <input type="submit" onClick={handleSubmit} value="Sign in" />
+          <input type="submit" onClick={HandleSubmit} value="Sign in" />
           <span><Link to="/signup" className="link2">Create new account?</Link></span>
         </form>
       </div>
