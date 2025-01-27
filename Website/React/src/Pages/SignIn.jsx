92fe65b9aa86a944d2343da2ea4d07a714a53cda
Toth Zoltan <tot.zol0226@gmail.com>
2025-01-27 16:41:02 +0100

    2025-01-27 16:41:02

--- Website/React/src/Pages/SignIn.jsx
+++ Website/React/src/Pages/SignIn.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from "axios"
-import { Link, resolvePath } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import "./SignIn.css"
 
 export default function SignIn() {
