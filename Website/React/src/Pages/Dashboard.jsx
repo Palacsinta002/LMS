821fac265809cd5a6f0d3868458ddb412f2820f4
Toth Zoltan <tot.zol0226@gmail.com>
2025-02-12 20:41:34 +0100

    2025-02-12 20:41:34

--- Website/React/src/Pages/Dashboard.jsx
+++ Website/React/src/Pages/Dashboard.jsx
@@ -2,33 +2,7 @@ import React from 'react'
 
 export default function Dashboard() {
 
-  const borrowedBooks = [];
-
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Due Date</th>
-          <th>Status</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {borrowedBooks.map((book) => (
-          <tr key={book.id}>
-            <td >{book.title}</td>
-            <td >{book.dueDate}</td>
-            <td>
-              {book.status}
-            </td>
-            <td>
-              <button>Renew</button>
-              <button >Return</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div></div>
   )
 }
