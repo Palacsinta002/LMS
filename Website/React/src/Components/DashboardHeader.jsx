7a12acafffa6871070cb3aeed6e81e6fecccf89e
Tóth Zoltán <78497888+PhantoMan26@users.noreply.github.com>
2025-02-20 14:02:58 +0100

    2025-02-20 14:02:57

--- Website/React/src/Components/DashboardHeader.jsx
+++ Website/React/src/Components/DashboardHeader.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function DashboardHeader() {
+    /*const [name, setName] = useState();
+    useEffect(() =>{
+        axios.get()
+    }, [])*/
+    const name = "Johndoe"    
     return (
-        <div></div>
+        <div>
+            <h1>Hi, {name}</h1>
+            <nav>
+                <Link to="/">Dashboard</Link>
+                <Link to="/borrowings">Borrowings</Link>
+                <Link to="/reservations">Reservations</Link>
+                <Link to="/profile">Profile</Link>
+            </nav>
+        </div>
     )
 }
--- Website/React/src/Pages/Dashboard.jsx
+++ Website/React/src/Pages/Dashboard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import DashboardHeader from '../Components/DashboardHeader';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function Dashboard() {
 
   return (
-    <div></div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/borrowings' element={<Borrowings />}/>
+          <Route path='/reservations'element={<Reservations />}/>
+          <Route path='/profile' element={<Profile />}/>
+        </Routes>
+      </BrowserRouter>
+      <DashboardHeader />
+    </div>
   )
 }
