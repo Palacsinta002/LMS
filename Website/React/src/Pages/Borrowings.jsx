7db80113c1b705342067b51a42b241a568e0d17b
unknown <tot.zol0226@gmail.com>
2025-02-26 09:57:39 +0100

    2025-02-26 09:57:39

--- Website/React/src/Pages/Borrowings.jsx
+++ Website/React/src/Pages/Borrowings.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Borrowings() {
+  const location = useLocation()
+
+  const hidden = location.pathname === '/dashboard/borrowings' ? '' : 'hidden'
+
   return (
-    <div>Borrowings</div>
+    <div className={hidden}>Borrowings</div>
   )
 }
--- Website/React/src/Pages/Dashboard.jsx
+++ Website/React/src/Pages/Dashboard.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import DashboardHeader from '../Components/DashboardHeader';
+import Borrowings from './Borrowings';
+import Reservations from './Reservations';
+import Charts from './Charts';
+import Profile from './Profile';
 
 export default function Dashboard() {
 
   return (
     <div>
       <DashboardHeader />
+      <Borrowings />
+      <Reservations />
+      <Charts />
+      <Profile />
     </div>
   )
 }
