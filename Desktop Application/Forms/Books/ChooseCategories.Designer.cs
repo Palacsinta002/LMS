cae8dcc913dd519d51ee9dbdd27ded6169f791c8
Vágvölgyi Máté <vagvolgyimate@gszi.edu.hu>
2025-03-14 08:02:31 +0100

    2025-03-14 08:02:31

--- Desktop Application/Forms/Books/ChooseCategories.Designer.cs
+++ Desktop Application/Forms/Books/ChooseCategories.Designer.cs
@@ -33,19 +33,17 @@ namespace Desktop_Application.Forms.Books
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseCategories));
             header = new Panel();
             title = new Label();
-            close_btn = new Desktop_Application.Components.RoundedButton();
+            close_btn = new Components.RoundedButton();
             allCategories_grd = new DataGridView();
-            allAuthors_author = new DataGridViewTextBoxColumn();
-            allAuthors_books = new DataGridViewTextBoxColumn();
             selectedCategories_grd = new DataGridView();
-            selectedAuthors_Author = new DataGridViewTextBoxColumn();
-            selectedAuthors_books = new DataGridViewTextBoxColumn();
-            rightArrow_btn = new Desktop_Application.Components.RoundedButton();
-            leftArrow_btn = new Desktop_Application.Components.RoundedButton();
-            save = new Desktop_Application.Components.RoundedButton();
-            cancel = new Desktop_Application.Components.RoundedButton();
+            rightArrow_btn = new Components.RoundedButton();
+            leftArrow_btn = new Components.RoundedButton();
+            save = new Components.RoundedButton();
+            cancel = new Components.RoundedButton();
             allCategories_src = new System.Windows.Forms.TextBox();
             selectedCategories_src = new System.Windows.Forms.TextBox();
+            allCategories_category = new DataGridViewTextBoxColumn();
+            selectedCategories_category = new DataGridViewTextBoxColumn();
             header.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)allCategories_grd).BeginInit();
             ((System.ComponentModel.ISupportInitialize)selectedCategories_grd).BeginInit();
@@ -57,18 +55,17 @@ namespace Desktop_Application.Forms.Books
             header.Controls.Add(title);
             header.Controls.Add(close_btn);
             header.Location = new Point(1, 1);
-            header.Margin = new Padding(3, 4, 3, 4);
             header.Name = "header";
-            header.Size = new Size(1141, 73);
+            header.Size = new Size(998, 55);
             header.TabIndex = 62;
             // 
             // title
             // 
             title.AutoSize = true;
             title.Font = new Font("Yu Gothic UI Semibold", 16F);
-            title.Location = new Point(457, 13);
+            title.Location = new Point(400, 10);
             title.Name = "title";
-            title.Size = new Size(245, 37);
+            title.Size = new Size(199, 30);
             title.TabIndex = 2;
             title.Text = "Choose Categories";
             // 
@@ -88,10 +85,9 @@ namespace Desktop_Application.Forms.Books
             close_btn.Font = new Font("Yu Gothic UI Semibold", 14F);
             close_btn.ForeColor = Color.White;
             close_btn.Image = (Image)resources.GetObject("close_btn.Image");
-            close_btn.Location = new Point(1088, 12);
-            close_btn.Margin = new Padding(3, 4, 3, 4);
+            close_btn.Location = new Point(952, 9);
             close_btn.Name = "close_btn";
-            close_btn.Size = new Size(41, 47);
+            close_btn.Size = new Size(36, 35);
             close_btn.TabIndex = 25;
             close_btn.TabStop = false;
             close_btn.TextColor = Color.White;
@@ -107,9 +103,8 @@ namespace Desktop_Application.Forms.Books
             allCategories_grd.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             allCategories_grd.BackgroundColor = Color.White;
             allCategories_grd.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            allCategories_grd.Columns.AddRange(new DataGridViewColumn[] { allAuthors_author, allAuthors_books });
-            allCategories_grd.Location = new Point(14, 129);
-            allCategories_grd.Margin = new Padding(3, 4, 3, 4);
+            allCategories_grd.Columns.AddRange(new DataGridViewColumn[] { allCategories_category });
+            allCategories_grd.Location = new Point(12, 97);
             allCategories_grd.Name = "allCategories_grd";
             allCategories_grd.ReadOnly = true;
             allCategories_grd.RowHeadersVisible = false;
@@ -119,24 +114,8 @@ namespace Desktop_Application.Forms.Books
             allCategories_grd.ShowCellToolTips = false;
             allCategories_grd.ShowEditingIcon = false;
             allCategories_grd.ShowRowErrors = false;
-            allCategories_grd.Size = new Size(518, 583);
+            allCategories_grd.Size = new Size(453, 437);
             allCategories_grd.TabIndex = 63;
-            // 
-            // allAuthors_author
-            // 
-            allAuthors_author.HeaderText = "Author";
-            allAuthors_author.MinimumWidth = 6;
-            allAuthors_author.Name = "allAuthors_author";
-            allAuthors_author.ReadOnly = true;
-            allAuthors_author.Width = 83;
-            // 
-            // allAuthors_books
-            // 
-            allAuthors_books.HeaderText = "Book(s)";
-            allAuthors_books.MinimumWidth = 6;
-            allAuthors_books.Name = "allAuthors_books";
-            allAuthors_books.ReadOnly = true;
-            allAuthors_books.Width = 88;
             // 
             // selectedCategories_grd
             // 
@@ -148,9 +127,8 @@ namespace Desktop_Application.Forms.Books
             selectedCategories_grd.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             selectedCategories_grd.BackgroundColor = Color.White;
             selectedCategories_grd.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            selectedCategories_grd.Columns.AddRange(new DataGridViewColumn[] { selectedAuthors_Author, selectedAuthors_books });
-            selectedCategories_grd.Location = new Point(611, 129);
-            selectedCategories_grd.Margin = new Padding(3, 4, 3, 4);
+            selectedCategories_grd.Columns.AddRange(new DataGridViewColumn[] { selectedCategories_category });
+            selectedCategories_grd.Location = new Point(535, 97);
             selectedCategories_grd.Name = "selectedCategories_grd";
             selectedCategories_grd.ReadOnly = true;
             selectedCategories_grd.RowHeadersVisible = false;
@@ -160,24 +138,8 @@ namespace Desktop_Application.Forms.Books
             selectedCategories_grd.ShowCellToolTips = false;
             selectedCategories_grd.ShowEditingIcon = false;
             selectedCategories_grd.ShowRowErrors = false;
-            selectedCategories_grd.Size = new Size(518, 583);
+            selectedCategories_grd.Size = new Size(453, 437);
             selectedCategories_grd.TabIndex = 64;
-            // 
-            // selectedAuthors_Author
-            // 
-            selectedAuthors_Author.HeaderText = "Author";
-            selectedAuthors_Author.MinimumWidth = 6;
-            selectedAuthors_Author.Name = "selectedAuthors_Author";
-            selectedAuthors_Author.ReadOnly = true;
-            selectedAuthors_Author.Width = 83;
-            // 
-            // selectedAuthors_books
-            // 
-            selectedAuthors_books.HeaderText = "Book(s)";
-            selectedAuthors_books.MinimumWidth = 6;
-            selectedAuthors_books.Name = "selectedAuthors_books";
-            selectedAuthors_books.ReadOnly = true;
-            selectedAuthors_books.Width = 88;
             // 
             // rightArrow_btn
             // 
@@ -191,10 +153,9 @@ namespace Desktop_Application.Forms.Books
             rightArrow_btn.Font = new Font("Yu Gothic UI Semibold", 14F);
             rightArrow_btn.ForeColor = Color.Black;
             rightArrow_btn.Image = (Image)resources.GetObject("rightArrow_btn.Image");
-            rightArrow_btn.Location = new Point(546, 335);
-            rightArrow_btn.Margin = new Padding(3, 4, 3, 4);
+            rightArrow_btn.Location = new Point(478, 251);
             rightArrow_btn.Name = "rightArrow_btn";
-            rightArrow_btn.Size = new Size(50, 50);
+            rightArrow_btn.Size = new Size(44, 38);
             rightArrow_btn.TabIndex = 68;
             rightArrow_btn.TextColor = Color.Black;
             rightArrow_btn.UseVisualStyleBackColor = false;
@@ -212,10 +173,9 @@ namespace Desktop_Application.Forms.Books
             leftArrow_btn.Font = new Font("Yu Gothic UI Semibold", 14F);
             leftArrow_btn.ForeColor = Color.Black;
             leftArrow_btn.Image = (Image)resources.GetObject("leftArrow_btn.Image");
-            leftArrow_btn.Location = new Point(546, 396);
-            leftArrow_btn.Margin = new Padding(3, 4, 3, 4);
+            leftArrow_btn.Location = new Point(478, 297);
             leftArrow_btn.Name = "leftArrow_btn";
-            leftArrow_btn.Size = new Size(50, 50);
+            leftArrow_btn.Size = new Size(44, 38);
             leftArrow_btn.TabIndex = 69;
             leftArrow_btn.TextColor = Color.Black;
             leftArrow_btn.UseVisualStyleBackColor = false;
@@ -233,10 +193,9 @@ namespace Desktop_Application.Forms.Books
             save.FlatStyle = FlatStyle.Flat;
             save.Font = new Font("Yu Gothic UI Semibold", 14F);
             save.ForeColor = Color.White;
-            save.Location = new Point(929, 739);
-            save.Margin = new Padding(3, 4, 3, 4);
+            save.Location = new Point(813, 554);
             save.Name = "save";
-            save.Size = new Size(97, 48);
+            save.Size = new Size(85, 36);
             save.TabIndex = 83;
             save.TabStop = false;
             save.Text = "Ok";
@@ -255,10 +214,9 @@ namespace Desktop_Application.Forms.Books
             cancel.FlatStyle = FlatStyle.Flat;
             cancel.Font = new Font("Yu Gothic UI Semibold", 14F);
             cancel.ForeColor = Color.Black;
-            cancel.Location = new Point(1032, 739);
-            cancel.Margin = new Padding(3, 4, 3, 4);
+            cancel.Location = new Point(903, 554);
             cancel.Name = "cancel";
-            cancel.Size = new Size(97, 48);
+            cancel.Size = new Size(85, 36);
             cancel.TabIndex = 84;
             cancel.Text = "Cancel";
             cancel.TextColor = Color.Black;
@@ -269,37 +227,49 @@ namespace Desktop_Application.Forms.Books
             allCategories_src.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             allCategories_src.BackColor = Color.White;
             allCategories_src.Font = new Font("Yu Gothic UI Semibold", 14F);
-            allCategories_src.Location = new Point(295, 82);
-            allCategories_src.Margin = new Padding(3, 4, 3, 4);
+            allCategories_src.Location = new Point(258, 62);
             allCategories_src.MaxLength = 19;
             allCategories_src.Name = "allCategories_src";
             allCategories_src.PlaceholderText = "Search...";
-            allCategories_src.Size = new Size(237, 39);
+            allCategories_src.Size = new Size(208, 32);
             allCategories_src.TabIndex = 85;
             allCategories_src.TabStop = false;
-            allCategories_src.TextChanged += SearchAllAuthors;
+            allCategories_src.TextChanged += SearchAllCategories;
             // 
             // selectedCategories_src
             // 
             selectedCategories_src.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             selectedCategories_src.BackColor = Color.White;
             selectedCategories_src.Font = new Font("Yu Gothic UI Semibold", 14F);
-            selectedCategories_src.Location = new Point(892, 82);
-            selectedCategories_src.Margin = new Padding(3, 4, 3, 4);
+            selectedCategories_src.Location = new Point(780, 62);
             selectedCategories_src.MaxLength = 19;
             selectedCategories_src.Name = "selectedCategories_src";
             selectedCategories_src.PlaceholderText = "Search...";
-            selectedCategories_src.Size = new Size(237, 39);
+            selectedCategories_src.Size = new Size(208, 32);
             selectedCategories_src.TabIndex = 86;
             selectedCategories_src.TabStop = false;
-            selectedCategories_src.TextChanged += SearchSelectedAuthors;
+            selectedCategories_src.TextChanged += SearchSelectedCategories;
+            // 
+            // allCategories_category
+            // 
+            allCategories_category.HeaderText = "Category";
+            allCategories_category.Name = "allCategories_category";
+            allCategories_category.ReadOnly = true;
+            allCategories_category.Width = 80;
+            // 
+            // selectedCategories_category
+            // 
+            selectedCategories_category.HeaderText = "Category";
+            selectedCategories_category.Name = "selectedCategories_category";
+            selectedCategories_category.ReadOnly = true;
+            selectedCategories_category.Width = 80;
             // 
             // ChooseCategories
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1143, 800);
+            ClientSize = new Size(1000, 600);
             Controls.Add(selectedCategories_src);
             Controls.Add(allCategories_src);
             Controls.Add(save);
@@ -310,7 +280,6 @@ namespace Desktop_Application.Forms.Books
             Controls.Add(allCategories_grd);
             Controls.Add(header);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "ChooseCategories";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "ChooseBooks";
@@ -336,9 +305,7 @@ namespace Desktop_Application.Forms.Books
         private Components.RoundedButton cancel;
         private System.Windows.Forms.TextBox allCategories_src;
         private System.Windows.Forms.TextBox selectedCategories_src;
-        private DataGridViewTextBoxColumn allAuthors_author;
-        private DataGridViewTextBoxColumn allAuthors_books;
-        private DataGridViewTextBoxColumn selectedAuthors_Author;
-        private DataGridViewTextBoxColumn selectedAuthors_books;
+        private DataGridViewTextBoxColumn allCategories_category;
+        private DataGridViewTextBoxColumn selectedCategories_category;
     }
 }
