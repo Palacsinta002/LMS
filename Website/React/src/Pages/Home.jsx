e8b3fdcb43e9ef1e95e838cb58c89e1696f8fb2b
unknown <tot.zol0226@gmail.com>
2025-03-14 12:23:38 +0100

    2025-03-14 12:23:38

--- Website/React/src/Pages/Home.jsx
+++ Website/React/src/Pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../Components/Main'
 import HeroBrowseBooks from '../Components/HeroBrowseBooks'
 import "./Home.css"
+import Cards from '../Components/Cards';
+import axios from 'axios';
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/books")
+      .then(response => {
+        console.log(response.data)
+      })
+  })
+
   return (
     <div className="HeroSection">
       <section className="Hero">
@@ -11,6 +22,9 @@ export default function Home() {
       </section>
       <section className="BrowseBooks">
         <HeroBrowseBooks />
+        {books.map((item) => (
+          <Cards index={item.id} img={item.isbn} title={item.title} publisher={item.publisher} author={item.author} publicationYear={item.publicationYear} />
+        ))}
       </section>
     </div>
   )
