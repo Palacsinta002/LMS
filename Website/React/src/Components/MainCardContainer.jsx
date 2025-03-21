c8d4502680f1d68244058fd4f9bd8f25a99b5eea
unknown <tot.zol0226@gmail.com>
2025-03-21 15:21:18 +0100

    2025-03-21 15:21:18

--- Website/React/src/Components/MainCardContainer.jsx
+++ Website/React/src/Components/MainCardContainer.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import "../index.css"
-
-const cardData = [
-  { icon: "fa-bookmark", title: "Books", count: 123 },
-  { icon: "fa-search", title: "Members", count: 123 },
-  { icon: "fa-user", title: "Borrows", count: 123 },
-];
+import { useEffect, useState } from 'react';
 
 export default function MainCardContainer() {
+  const cardData = [
+    { icon: "fa-bookmark", title: "Books", count: 123 },
+    { icon: "fa-user", title: "Members", count: 123 },
+    { icon: "fa-search", title: "Borrows", count: 123 },
+  ];
+
+  const [books, setBooks] = useState("");
+  const [members, setMembers] = useState("");
+  const [borrows, setBorrows] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`/api/top-borrowings/5`);
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setError("Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="card-container">
       {cardData.map((card, index) => (
