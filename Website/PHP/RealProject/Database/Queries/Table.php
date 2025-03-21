b0f4f1676864e18bf0706ae07b36296891f89762
Zsomborgi Soma <zsomborgisoma@gmail.com>
2025-03-21 14:10:32 +0100

    2025-03-21 14:10:32

--- Website/PHP/RealProject/Database/Queries/Table.php
+++ Website/PHP/RealProject/Database/Queries/Table.php
@@ -92,8 +92,6 @@ class Table {
     protected static function execute( $getresult, $fetch = true ){
         $conn = Connection::connect();
         self::fixingValues();
-        echo self::$query;
-        die();
         $query = $conn->prepare(self::$query);
         if (substr_count(self::$query,"?") == 0 ){
             $query->execute();
