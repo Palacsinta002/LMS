b9dbe801bbed486fcf718a2514ba5023eff32aa1
unknown <tot.zol0226@gmail.com>
2025-03-21 15:39:27 +0100

    2025-03-21 15:39:27

--- Website/React/src/Components/MainCardContainer.jsx
+++ Website/React/src/Components/MainCardContainer.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function MainCardContainer() {
-  const [books, setBooks] = useState("");
-  const [members, setMembers] = useState("");
-  const [borrows, setBorrows] = useState("");
+  const [books, setBooks] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [borrows, setBorrows] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(""); 
+  const [error, setError] = useState("");
 
   const cardData = [
     { icon: "fa-bookmark", title: "Books", count: books },
@@ -36,7 +36,7 @@ export default function MainCardContainer() {
       try {
         const response = await axios.get(`/api/all-user`);
         console.log(response.data);
-        setBooks(response.data);
+        setMembers(response.data);
       } catch (error) {
         console.error("Error fetching books:", error);
         setError("Failed to fetch data");
@@ -51,7 +51,7 @@ export default function MainCardContainer() {
       try {
         const response = await axios.get(`/api/all-borrowings`);
         console.log(response.data);
-        setBooks(response.data);
+        setBorrows(response.data);
       } catch (error) {
         console.error("Error fetching books:", error);
         setError("Failed to fetch data");
@@ -64,11 +64,25 @@ export default function MainCardContainer() {
 
   return (
     <div className="card-container">
-      {cardData.map((card, index) => (
-        <div className="cards" key={index}>
-          <i className={`fa ${card.icon}`}></i>
-          <h2>{card.title}</h2>
-          <p>{card.count}</p>
+      {books.map((item, index) => (
+        <div key={index} className="cards">
+          <i className="fa-bookmark"></i>
+          <h2>Books</h2>
+          <p>{item.books}</p>
+        </div>
+      ))}
+      {members.map((item, index) => (
+        <div key={index} className="cards">
+          <i className="fa-user"></i>
+          <h2>Members</h2>
+          <p>{item.users}</p>
+        </div>
+      ))}
+      {borrows.map((item, index) => (
+        <div key={index} className="cards">
+          <i className="fa-search"></i>
+          <h2>Borrowings</h2>
+          <p>{item.borrowings}</p>
         </div>
       ))}
     </div>
