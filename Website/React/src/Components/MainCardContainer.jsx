78d9365428c79f0f1c80a6a78271713aa51745d3
unknown <tot.zol0226@gmail.com>
2025-03-21 15:28:05 +0100

    2025-03-21 15:28:05

--- Website/React/src/Components/MainCardContainer.jsx
+++ Website/React/src/Components/MainCardContainer.jsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import "../index.css"
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function MainCardContainer() {
-  const cardData = [
-    { icon: "fa-bookmark", title: "Books", count: 123 },
-    { icon: "fa-user", title: "Members", count: 123 },
-    { icon: "fa-search", title: "Borrows", count: 123 },
-  ];
-
   const [books, setBooks] = useState("");
   const [members, setMembers] = useState("");
   const [borrows, setBorrows] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(""); 
+
+  const cardData = [
+    { icon: "fa-bookmark", title: "Books", count: books },
+    { icon: "fa-user", title: "Members", count: members },
+    { icon: "fa-search", title: "Borrows", count: borrows },
+  ];
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get(`/api/top-borrowings/5`);
+        const response = await axios.get(`/api/all-books`);
         console.log(response.data);
-        setData(response.data);
+        setBooks(response.data);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setError("Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`/api/all-user`);
+        console.log(response.data);
+        setBooks(response.data);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setError("Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`/api/all-borrowings`);
+        console.log(response.data);
+        setBooks(response.data);
       } catch (error) {
         console.error("Error fetching books:", error);
         setError("Failed to fetch data");
