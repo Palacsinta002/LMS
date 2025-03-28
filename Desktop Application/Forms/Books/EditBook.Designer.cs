c3d417c0e67d2c2ec31a591ccc7753edaacc4c03
unknown <vagvolgyimate19@gmail.com>
2025-03-28 15:10:13 +0100

    2025-03-28 15:10:13

--- Desktop Application/Forms/Books/EditBook.Designer.cs
+++ Desktop Application/Forms/Books/EditBook.Designer.cs
@@ -47,14 +47,13 @@ namespace Desktop_Application.Forms.Books
             textBox_author = new TextBox();
             save = new RoundedButton();
             cancel = new RoundedButton();
-            label2 = new Label();
-            button_chooseImage = new Button();
-            textBox_image = new TextBox();
             label1 = new Label();
             dropDown_publisher = new ComboBox();
             textBox_isbn = new TextBox();
             label_isbn = new Label();
             label_publisher = new Label();
+            textBox_image = new TextBox();
+            button_browseImage = new RoundedButton();
             header.SuspendLayout();
             SuspendLayout();
             // 
@@ -62,9 +61,9 @@ namespace Desktop_Application.Forms.Books
             // 
             label_title.AutoSize = true;
             label_title.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label_title.Location = new Point(14, 97);
+            label_title.Location = new Point(12, 73);
             label_title.Name = "label_title";
-            label_title.Size = new Size(61, 32);
+            label_title.Size = new Size(49, 25);
             label_title.TabIndex = 3;
             label_title.Text = "Title";
             // 
@@ -72,19 +71,18 @@ namespace Desktop_Application.Forms.Books
             // 
             textBox_title.BackColor = Color.WhiteSmoke;
             textBox_title.Font = new Font("Yu Gothic UI Semibold", 14F);
-            textBox_title.Location = new Point(125, 93);
-            textBox_title.Margin = new Padding(3, 4, 3, 4);
+            textBox_title.Location = new Point(109, 70);
             textBox_title.Name = "textBox_title";
-            textBox_title.Size = new Size(433, 39);
+            textBox_title.Size = new Size(379, 32);
             textBox_title.TabIndex = 1;
             // 
             // label_author
             // 
             label_author.AutoSize = true;
             label_author.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label_author.Location = new Point(14, 199);
+            label_author.Location = new Point(12, 149);
             label_author.Name = "label_author";
-            label_author.Size = new Size(90, 32);
+            label_author.Size = new Size(72, 25);
             label_author.TabIndex = 12;
             label_author.Text = "Author";
             // 
@@ -92,9 +90,9 @@ namespace Desktop_Application.Forms.Books
             // 
             label_category.AutoSize = true;
             label_category.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label_category.Location = new Point(14, 249);
+            label_category.Location = new Point(12, 187);
             label_category.Name = "label_category";
-            label_category.Size = new Size(113, 32);
+            label_category.Size = new Size(90, 25);
             label_category.TabIndex = 14;
             label_category.Text = "Category";
             // 
@@ -102,9 +100,9 @@ namespace Desktop_Application.Forms.Books
             // 
             label_pubYear.AutoSize = true;
             label_pubYear.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label_pubYear.Location = new Point(14, 148);
+            label_pubYear.Location = new Point(12, 111);
             label_pubYear.Name = "label_pubYear";
-            label_pubYear.Size = new Size(189, 32);
+            label_pubYear.Size = new Size(150, 25);
             label_pubYear.TabIndex = 18;
             label_pubYear.Text = "Publication Year";
             // 
@@ -112,11 +110,10 @@ namespace Desktop_Application.Forms.Books
             // 
             textBox_pubYear.BackColor = Color.WhiteSmoke;
             textBox_pubYear.Font = new Font("Yu Gothic UI Semibold", 14F);
-            textBox_pubYear.Location = new Point(209, 145);
-            textBox_pubYear.Margin = new Padding(3, 4, 3, 4);
+            textBox_pubYear.Location = new Point(183, 109);
             textBox_pubYear.MaxLength = 4;
             textBox_pubYear.Name = "textBox_pubYear";
-            textBox_pubYear.Size = new Size(63, 39);
+            textBox_pubYear.Size = new Size(56, 32);
             textBox_pubYear.TabIndex = 2;
             // 
             // header
@@ -125,18 +122,17 @@ namespace Desktop_Application.Forms.Books
             header.Controls.Add(title);
             header.Controls.Add(close_btn);
             header.Location = new Point(2, 1);
-            header.Margin = new Padding(3, 4, 3, 4);
             header.Name = "header";
-            header.Size = new Size(568, 73);
+            header.Size = new Size(497, 55);
             header.TabIndex = 46;
             // 
             // title
             // 
             title.AutoSize = true;
             title.Font = new Font("Yu Gothic UI Semibold", 16F);
-            title.Location = new Point(216, 16);
+            title.Location = new Point(189, 12);
             title.Name = "title";
-            title.Size = new Size(133, 37);
+            title.Size = new Size(108, 30);
             title.TabIndex = 2;
             title.Text = "Edit Book";
             // 
@@ -155,10 +151,9 @@ namespace Desktop_Application.Forms.Books
             close_btn.Font = new Font("Yu Gothic UI Semibold", 14F);
             close_btn.ForeColor = Color.White;
             close_btn.Image = (Image)resources.GetObject("close_btn.Image");
-            close_btn.Location = new Point(514, 15);
-            close_btn.Margin = new Padding(3, 4, 3, 4);
+            close_btn.Location = new Point(450, 11);
             close_btn.Name = "close_btn";
-            close_btn.Size = new Size(41, 47);
+            close_btn.Size = new Size(36, 35);
             close_btn.TabIndex = 25;
             close_btn.TabStop = false;
             close_btn.TextColor = Color.White;
@@ -166,10 +161,9 @@ namespace Desktop_Application.Forms.Books
             // 
             // chooseCategory
             // 
-            chooseCategory.Location = new Point(515, 245);
-            chooseCategory.Margin = new Padding(3, 4, 3, 4);
+            chooseCategory.Location = new Point(451, 184);
             chooseCategory.Name = "chooseCategory";
-            chooseCategory.Size = new Size(42, 43);
+            chooseCategory.Size = new Size(37, 32);
             chooseCategory.TabIndex = 4;
             chooseCategory.Text = "...";
             chooseCategory.UseVisualStyleBackColor = true;
@@ -177,10 +171,9 @@ namespace Desktop_Application.Forms.Books
             // 
             // chooseAuthor
             // 
-            chooseAuthor.Location = new Point(515, 195);
-            chooseAuthor.Margin = new Padding(3, 4, 3, 4);
+            chooseAuthor.Location = new Point(451, 146);
             chooseAuthor.Name = "chooseAuthor";
-            chooseAuthor.Size = new Size(42, 43);
+            chooseAuthor.Size = new Size(37, 32);
             chooseAuthor.TabIndex = 3;
             chooseAuthor.Text = "...";
             chooseAuthor.UseVisualStyleBackColor = true;
@@ -191,10 +184,9 @@ namespace Desktop_Application.Forms.Books
             textBox_category.BackColor = Color.WhiteSmoke;
             textBox_category.Enabled = false;
             textBox_category.Font = new Font("Yu Gothic UI Semibold", 14F);
-            textBox_category.Location = new Point(125, 245);
-            textBox_category.Margin = new Padding(3, 4, 3, 4);
+            textBox_category.Location = new Point(109, 184);
             textBox_category.Name = "textBox_category";
-            textBox_category.Size = new Size(383, 39);
+            textBox_category.Size = new Size(336, 32);
             textBox_category.TabIndex = 50;
             // 
             // textBox_author
@@ -202,10 +194,9 @@ namespace Desktop_Application.Forms.Books
             textBox_author.BackColor = Color.WhiteSmoke;
             textBox_author.Enabled = false;
             textBox_author.Font = new Font("Yu Gothic UI Semibold", 14F);
-            textBox_author.Location = new Point(125, 195);
-            textBox_author.Margin = new Padding(3, 4, 3, 4);
+            textBox_author.Location = new Point(109, 146);
             textBox_author.Name = "textBox_author";
-            textBox_author.Size = new Size(383, 39);
+            textBox_author.Size = new Size(336, 32);
             textBox_author.TabIndex = 49;
             // 
             // save
@@ -221,10 +212,9 @@ namespace Desktop_Application.Forms.Books
             save.FlatStyle = FlatStyle.Flat;
             save.Font = new Font("Yu Gothic UI Semibold", 14F);
             save.ForeColor = Color.White;
-            save.Location = new Point(185, 450);
-            save.Margin = new Padding(3, 4, 3, 4);
+            save.Location = new Point(162, 348);
             save.Name = "save";
-            save.Size = new Size(97, 48);
+            save.Size = new Size(85, 36);
             save.TabIndex = 79;
             save.TabStop = false;
             save.Text = "Save";
@@ -244,55 +234,22 @@ namespace Desktop_Application.Forms.Books
             cancel.FlatStyle = FlatStyle.Flat;
             cancel.Font = new Font("Yu Gothic UI Semibold", 14F);
             cancel.ForeColor = Color.Black;
-            cancel.Location = new Point(288, 450);
-            cancel.Margin = new Padding(3, 4, 3, 4);
+            cancel.Location = new Point(252, 348);
             cancel.Name = "cancel";
-            cancel.Size = new Size(97, 48);
+            cancel.Size = new Size(85, 36);
             cancel.TabIndex = 80;
             cancel.TabStop = false;
             cancel.Text = "Cancel";
             cancel.TextColor = Color.Black;
             cancel.UseVisualStyleBackColor = false;
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label2.Location = new Point(325, 296);
-            label2.Name = "label2";
-            label2.Size = new Size(168, 32);
-            label2.TabIndex = 92;
-            label2.Text = "Add book pics";
-            // 
-            // button_chooseImage
-            // 
-            button_chooseImage.Location = new Point(515, 292);
-            button_chooseImage.Margin = new Padding(3, 4, 3, 4);
-            button_chooseImage.Name = "button_chooseImage";
-            button_chooseImage.Size = new Size(42, 43);
-            button_chooseImage.TabIndex = 85;
-            button_chooseImage.Text = "...";
-            button_chooseImage.UseVisualStyleBackColor = true;
-            // 
-            // textBox_image
-            // 
-            textBox_image.BackColor = Color.WhiteSmoke;
-            textBox_image.Enabled = false;
-            textBox_image.Font = new Font("Yu Gothic UI Semibold", 14F);
-            textBox_image.Location = new Point(125, 292);
-            textBox_image.Margin = new Padding(3, 4, 3, 4);
-            textBox_image.Name = "textBox_image";
-            textBox_image.Size = new Size(383, 39);
-            textBox_image.TabIndex = 91;
-            textBox_image.TabStop = false;
-            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label1.Location = new Point(14, 296);
+            label1.Location = new Point(12, 228);
             label1.Name = "label1";
-            label1.Size = new Size(82, 32);
+            label1.Size = new Size(66, 25);
             label1.TabIndex = 90;
             label1.Text = "Image";
             // 
@@ -302,31 +259,29 @@ namespace Desktop_Application.Forms.Books
             dropDown_publisher.DropDownStyle = ComboBoxStyle.DropDownList;
             dropDown_publisher.Font = new Font("Yu Gothic UI Semibold", 14F);
             dropDown_publisher.FormattingEnabled = true;
-            dropDown_publisher.Location = new Point(126, 339);
-            dropDown_publisher.Margin = new Padding(3, 4, 3, 4);
+            dropDown_publisher.Location = new Point(108, 264);
             dropDown_publisher.MaxDropDownItems = 10;
             dropDown_publisher.Name = "dropDown_publisher";
-            dropDown_publisher.Size = new Size(433, 39);
+            dropDown_publisher.Size = new Size(379, 33);
             dropDown_publisher.TabIndex = 86;
             // 
             // textBox_isbn
             // 
             textBox_isbn.BackColor = Color.WhiteSmoke;
             textBox_isbn.Font = new Font("Yu Gothic UI Semibold", 14F);
-            textBox_isbn.Location = new Point(126, 391);
-            textBox_isbn.Margin = new Padding(3, 4, 3, 4);
+            textBox_isbn.Location = new Point(108, 303);
             textBox_isbn.MaxLength = 13;
             textBox_isbn.Name = "textBox_isbn";
-            textBox_isbn.Size = new Size(173, 39);
+            textBox_isbn.Size = new Size(152, 32);
             textBox_isbn.TabIndex = 87;
             // 
             // label_isbn
             // 
             label_isbn.AutoSize = true;
             label_isbn.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label_isbn.Location = new Point(15, 395);
+            label_isbn.Location = new Point(12, 306);
             label_isbn.Name = "label_isbn";
-            label_isbn.Size = new Size(67, 32);
+            label_isbn.Size = new Size(54, 25);
             label_isbn.TabIndex = 89;
             label_isbn.Text = "ISBN";
             // 
@@ -334,20 +289,52 @@ namespace Desktop_Application.Forms.Books
             // 
             label_publisher.AutoSize = true;
             label_publisher.Font = new Font("Yu Gothic UI Semibold", 14F);
-            label_publisher.Location = new Point(15, 343);
+            label_publisher.Location = new Point(12, 267);
             label_publisher.Name = "label_publisher";
-            label_publisher.Size = new Size(114, 32);
+            label_publisher.Size = new Size(91, 25);
             label_publisher.TabIndex = 88;
             label_publisher.Text = "Publisher";
             // 
+            // textBox_image
+            // 
+            textBox_image.BackColor = Color.WhiteSmoke;
+            textBox_image.Enabled = false;
+            textBox_image.Font = new Font("Yu Gothic UI Semibold", 14F);
+            textBox_image.Location = new Point(217, 225);
+            textBox_image.Name = "textBox_image";
+            textBox_image.RightToLeft = RightToLeft.Yes;
+            textBox_image.Size = new Size(270, 32);
+            textBox_image.TabIndex = 91;
+            textBox_image.TabStop = false;
+            // 
+            // button_browseImage
+            // 
+            button_browseImage.BackColor = Color.WhiteSmoke;
+            button_browseImage.BackgroundColor = Color.WhiteSmoke;
+            button_browseImage.BorderColor = Color.FromArgb(80, 77, 180);
+            button_browseImage.BorderRadius = 5;
+            button_browseImage.BorderSize = 2;
+            button_browseImage.FlatAppearance.BorderSize = 0;
+            button_browseImage.FlatStyle = FlatStyle.Flat;
+            button_browseImage.Font = new Font("Yu Gothic UI Semibold", 14F);
+            button_browseImage.ForeColor = Color.Black;
+            button_browseImage.Location = new Point(109, 222);
+            button_browseImage.Name = "button_browseImage";
+            button_browseImage.Size = new Size(102, 36);
+            button_browseImage.TabIndex = 92;
+            button_browseImage.TabStop = false;
+            button_browseImage.Text = "Browse...";
+            button_browseImage.TextColor = Color.Black;
+            button_browseImage.UseVisualStyleBackColor = false;
+            button_browseImage.Click += SelectImage;
+            // 
             // EditBook
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(570, 510);
-            Controls.Add(label2);
-            Controls.Add(button_chooseImage);
+            ClientSize = new Size(499, 392);
+            Controls.Add(button_browseImage);
             Controls.Add(textBox_image);
             Controls.Add(label1);
             Controls.Add(dropDown_publisher);
@@ -370,7 +357,6 @@ namespace Desktop_Application.Forms.Books
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             KeyPreview = true;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "EditBook";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "EditBook";
@@ -397,13 +383,12 @@ namespace Desktop_Application.Forms.Books
         private TextBox textBox_author;
         private RoundedButton save;
         private RoundedButton cancel;
-        private Label label2;
-        private Button button_chooseImage;
-        private TextBox textBox_image;
         private Label label1;
         private ComboBox dropDown_publisher;
         private TextBox textBox_isbn;
         private Label label_isbn;
         private Label label_publisher;
+        private TextBox textBox_image;
+        private RoundedButton button_browseImage;
     }
 }
