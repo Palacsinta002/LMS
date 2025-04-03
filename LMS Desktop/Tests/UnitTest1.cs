80b08cc459866abae17ed15fed5d8eb31645ddf9
Kézigránát <vagvolgyimate19@gmail.com>
2025-04-03 12:53:01 +0200

    2025-04-03 12:53:01

--- LMS Desktop/Tests/UnitTest1.cs
+++ LMS Desktop/Tests/UnitTest1.cs
@@ -5,6 +5,8 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            string query = $"SELECT Users.Password, Roles.Role FROM Users JOIN Roles ON Roles.id = Users.RoleID WHERE Username = '{_username}'";
+            var result = HandleQueries.SelectFromString(query);
         }
 
         [Test]
