2c5e62d1fd4f47b23ca8e1e18efc54b1a462ade0
Kézigránát <vagvolgyimate19@gmail.com>
2025-04-04 10:09:12 +0200

    2025-04-04 10:09:11

--- LMS Desktop/Publishes/SqlQueries/SelectCurrentBorrowing.sql
+++ LMS Desktop/Publishes/SqlQueries/SelectCurrentBorrowing.sql
@@ -2,10 +2,9 @@
     Users.Username,
     Books.Title,
     Borrowings.ISBN,
-    DATE_FORMAT(Borrowings.BorrowDate, "%d %M %Y"),
-    DATE_FORMAT(Borrowings.DueDate, "%d %M %Y"),
-    DATE_FORMAT(Borrowings.ReturnDate, "%d %M %Y")
+    DATE_FORMAT(Borrowings.BorrowDate, "%d/%m/%Y"),
+    DATE_FORMAT(Borrowings.DueDate, "%d/%m/%Y")
 FROM Borrowings
 JOIN Users ON Users.id = Borrowings.UserID
 JOIN Books ON Books.ISBN = Borrowings.ISBN
-ORDER BY BorrowDate DESC, ISBN ASC;
+ORDER BY BorrowDate DESC;
